57aacdef147638a85e469267bb8eff19c903cf77
bubleM <migunalina53@gmail.com>
2025-08-24 00:16:50 +0300

    Add files via upload

---
+++ kpo_meow/lab3/test/test/stdafx.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include <string>
+using namespace std;
+extern vector<pair<string, int>> months;
+extern vector<pair<string, int>> holidays;
+#include "getnumber.h"
+#include "isleap.h"
