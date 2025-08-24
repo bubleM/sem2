50a42a715446ff6b3a84f21e91e8fc0feb495b3b
bubleM <migunalina53@gmail.com>
2025-08-24 14:49:07 +0300

    Add files via upload

---
+++ оап/lab10/var12/var12/var12.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+double recurs(int n, double x) {
+
+	if (n == 0)
+		return 0;
+	else
+		return cos(n * x) + recurs(n - 1, x);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	double y;
+	int n;
+	double x;
+	cout << "введите n= ";
+	cin >> n;
+	cout << "введите угол x= ";
+	cin >> x;
+	y = recurs(n, x);
+	cout << y;
+}
