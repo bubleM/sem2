07513faa956dea7f575fb56e246a80361265b02d
bubleM <migunalina53@gmail.com>
2025-08-24 15:23:16 +0300

    Add files via upload

---
+++ оап/lab10/dop2/dop2/dop2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int printP(int arr[], int nool, int n)
+{
+	if (nool == n)
+	{
+		for (int i = 0; i != n; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		return arr[n];
+	}
+	for (int i = nool; i != n; i++) {
+		swap(arr[nool], arr[i]);
+
+		printP(arr, nool + 1, n);
+		swap(arr[nool], arr[i]);
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int n = 5;
+	int arr[5];
+
+	for (int i = 0; i != 5; i++)
+	{
+		cout << "Введите " << i + 1 << "  элемент: ";
+		cin >> arr[i];
+	}
+
+	cout << "Все перестановки:" << endl;
+	printP(arr, 0, n);
+}
---
+++ оап/lab10/var10/var10/var10.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int getFunction( int m, int n)
+{
+	if (m == 0 || n == 0)
+		return n + 1;
+	else
+		return getFunction(m - 1, getFunction(m, n - 1));
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int F;
+	int n, m;
+
+	cout<<"Введите 2 целых неотрицательных числа: "<<endl;
+	cout << "n= ";
+	cin >> n;
+	if (n < 0) {
+		cout << "попробуйте еще раз!";
+		return 1;
+	}
+
+	cout << "m= ";
+	cin >> m;
+	if (m < 0) {
+		cout << "попробуйте еще раз!";
+		return 1;
+	}
+
+	F = getFunction(m,n);
+	cout <<"F=" << F;
+}
