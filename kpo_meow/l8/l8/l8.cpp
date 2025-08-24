1e1b39b3f17e48078beac2db1a6373b5e8a2d108
bubleM <migunalina53@gmail.com>
2025-08-24 13:51:23 +0300

    Add files via upload

---
+++ kpo_meow/l8/l8/l8.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int defaultparm(int a, int b, int c, int d, int e, int f = 0, int g = 0) {
+    return (a + b + c + d + e + f + g) / 7;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int avg1 = defaultparm(1, 2, 3, 4, 5);
+    int avg2 = defaultparm(1, 2, 3, 4, 5, 6, 7);
+    cout << "первое: " << avg1 << endl; // (1+2+3+4+5+0+0)/7 = 2
+    cout << "второе: " << avg2 << endl; // (1+2+3+4+5+6+7)/7 = 4
+}
